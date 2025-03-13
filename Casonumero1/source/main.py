cef54e91821ab9e70aa4e9bb2c4ca6e490ea989b
daferarte <daferarte@gmail.com>
2025-03-13 08:10:12 -0500

    ejecutando codigo main

--- Casonumero1/source/main.py
+++ Casonumero1/source/main.py
@@ -10,3 +10,6 @@ print(f'el nuevo salario del empleado es de: {empleado1.DarSalario()}')
 print(f'el salario anual del empleado es de: {empleado1.DarSalarioAnual()}')
 
 print(f'el impuesto anual es de: {empleado1.CalcularImpuestoAnual()}')
+
+empleado1.CambiarFechaIngreso(28,5,2015)
+print(f'El empleado ingreso a la empresa en la fecha: {empleado1.DarFechaIngreso()}')
