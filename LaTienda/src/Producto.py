23798c0cf670781272a9a2d7e5fba94126b11c24
daferarte <daferarte@gmail.com>
2025-04-08 08:44:18 -0500

    condicionales

--- LaTienda/src/Producto.py
+++ LaTienda/src/Producto.py
@@ -72,3 +72,79 @@ class Producto:
 
         # Forma2
         return (self.__valorUnitario*constantes.IVA_SUPERMERCADO)+self.__valorUnitario
+    
+    
+    __method__ = "Vender"
+    __params__ = "cantidad"
+    __returns__ = "Mensaje de confirmacion"
+    __descriptions__ = "Metodo que sirve para vender productos"
+    def Vender(self, cantidad:int):
+        # aqui va el codigo
+        if cantidad <= self.__cantidadBodega:
+            self.__unidadesVendidas += cantidad
+            self.__cantidadBodega -= cantidad
+            return 'Venta exitosa'
+        else:
+            return f'No se cuenta con {cantidad} de unidades disponibles de {self.__nombre} para su venta'
+        
+    __method__ = "HaySuficiente"
+    __params__ = "cantidad"
+    __returns__ = "suficiente"
+    __descriptions__ = "Metodo que sirve para verificar si existen suficientes productos"
+    def HaySuficiente(self, cantidad):
+        # aqui va el codigo
+        # Forma 1
+        # suficiente:bool
+        
+        # if cantidad <= self.__cantidadBodega:
+        #     suficiente = True
+        # else: 
+        #     suficiente = False
+            
+        # return suficiente
+        
+        # Forma 2
+        # suficiente:bool = False
+        
+        # if cantidad <= self.__cantidadBodega:
+        #     suficiente = True 
+        
+        # return suficiente
+        
+        # Forma 3
+        return cantidad <= self.__cantidadBodega
+    
+    __method__ = "DarPrecioPapeleria"
+    __params__ = "conIVA"
+    __returns__ = "precio"
+    __descriptions__ = "Metodo que sirve para retornar el precio de un producto"
+    def DarPrecioPapeleria(self, conIVA:bool):
+        precioFinal:float = self.__valorUnitario
+        
+        if conIVA:
+            precioFinal *= (1+constantes.IVA_PAPELERIA)
+            
+        return precioFinal
+    
+    __method__ = "AjustarPrecio"
+    __params__ = "ninguno"
+    __returns__ = "nada"
+    __descriptions__ = "Metodo que sirve para ajustar el precio de un producto"
+    def AjustarPrecio(self):
+        if self.__unidadesVendidas < 100:
+            self.__valorUnitario *= (80/100)
+        else:
+            self.__valorUnitario *= 1.1
+    
+    __method__ = "DarIva"
+    __params__ = "ninguno"
+    __returns__ = "IVA producto"
+    __descriptions__ = "Metodo que sirve para conocer el iva de un producto"
+    def DarIva(self):
+        
+        if self.__tipo== Tipo.DROGUERIA:
+            return constantes.IVA_DROGUERIA
+        elif self.__tipo== Tipo.PAPELERIA:
+            return constantes.IVA_PAPELERIA
+        else:
+            return constantes.IVA_SUPERMERCADO
